24af904ca13ff201919b4c618247e09070958e1a
Jadson Pessoa <jadsonpessoa@gmail.com>
2023-06-30 17:17:00 -0300

    repositorios

---
+++ Sec02_Tratamento_Analise_Exploratoria/01_Extracao_Repositorios.R
@@ -0,0 +1,41 @@
+##########################################################
+###    EXTRAÇÃO DE DADOS REPOSITÓRIO                   ###
+##########################################################
+
+# 
+
+# Repositórios Economia ---------------------------------------------------
+
+## IBGE: https://www.ibge.gov.br/estatisticas/downloads-estatisticas.html
+## BCB: https://www3.bcb.gov.br/sgspub/localizarseries/localizarSeries.do?method=prepararTelaLocalizarSeries
+## IPEA: http://www.ipeadata.gov.br/Default.aspx
+## MTE: https://bi.mte.gov.br/bgcaged/login.php
+## DATAZOOM: https://www.econ.puc-rio.br/datazoom/index.html
+## CBID: http://www.cbicdados.com.br/menu/pib-e-investimento/pib-brasil-e-construcao-civil
+
+
+# Outros Repositórios  ----------------------------------------------------
+
+## INEP: https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados 
+## Google dataset Search: https://datasetsearch.research.google.com/ 
+## Portal brasileiro de dados abertos: www.dados.gov.br 
+## Kaggle: (competições Machine Learning): www.kaggle.com 
+## OMS: https://www.who.int/
+
+
+# Pacotes para baixar dados (API) -----------------------------------------
+
+install.packages("NOME_PACOTE")
+
+## ipeadatar: Dados do IPEADATA
+## rbcb: Dados do Banco +Central do Brasil
+## BETS: Dados financeiros da B3
+## PNADcIBGE: PNAD continua
+## geobr: Dados para criação de mapas (IPEA)
+## electionBR: Dados eleitorais do Brasil
+## cepR: dados postais do Brasil
+## GetTDData: Tesouro do governo brasileiro
+## Quandl: Dados financeiros de companhias (intenacional) 
+
+
+ 
