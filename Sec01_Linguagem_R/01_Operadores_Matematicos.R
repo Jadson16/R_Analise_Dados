8f96a33de8d2c9c18fdc0009ad14ddecf0bf7643
Jadson Pessoa <jadsonpessoa@gmail.com>
2023-06-30 10:15:50 -0300

    operadores matemáticos

---
+++ Sec01_Linguagem_R/01_Operadores_Matematicos.R
@@ -0,0 +1,108 @@
+#########################################
+#
+#  OPERADORES MATEMÁTICOS
+#
+##########################################
+
+
+# Representação decimal no R é o ponto (.) e não a vírgula (,)
+
+# R como uma calculadora -------------------------------------
+
+# Soma   (linha de comentário)
+5 + 3
+
+# Subtração
+8 - 6
+
+# Multiplicação
+4 * 3
+
+# Divisão
+20 / 5
+
+# Potenciação
+4 ** 2
+
+# ou
+4 ^ 2
+
+
+# Algumas funções matemáticas comuns já estão prontas para o uso: ---------
+
+
+# raiz quadrada
+sqrt (81)
+
+# Módulo
+abs (-10)
+abs (10)
+
+# Fatorial
+
+factorial(3)   # 3! = 3.2.1 = 6
+factorial(5)   # 5! = 5.4.3.2.1 = 120
+factorial(17)  # 17!
+
+# Logaritmo (base 2 e base 10)
+log10(100)    # 10^2 = 100
+
+log2(8)       # 2^3 = 8
+
+log10(1000)
+
+# Exponencial
+exp(1)       # e = 2,718281828...
+exp(2)
+exp(-2)
+
+# Logaritmo natural ou neperiano
+log(2.718281828)
+log(100)
+
+
+
+# Funções trigonométricas (radianos) ------------------------------
+## pi radianos = 180 graus
+## Constante pi
+pi
+
+
+sin(pi/6)  # pi/6 radianos é igual a 30 graus
+asin(0.5)
+
+cos(pi/4)  # pi/4 radianos é igual a 45 graus
+acos(0.7071068)
+
+tan(pi/4)
+atan(1)
+
+
+# Notação científica ------------------------------------------------------
+
+2e32 # (2x10^32)
+
+2e3 # 2x10^3
+
+2e-26
+
+2e5 + 4e5
+
+3e20 * 2e12
+
+15e16 / 5e6
+
+# Funções para arredondamento -------------------------------
+ceiling(4.3478) # teto
+
+floor(4.3478) # chão
+
+round(4.3478) # arredonda com nenhuma casa decimal
+
+round(4.3478,3) # arredonda com 3 casas decimais
+
+round(4.3478,2) # arredonda com 2 casas decimais
+  
+  
+  
+  
